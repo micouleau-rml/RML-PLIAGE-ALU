2dc91dce2970b177fc1229a55bc8ce5e9fbcd7a0
micouleau-rml <contact.rml@orange.fr>
2026-08-20 09:38:35 +0200

    Publication RML Pliage ALU V69

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rml-pliage-v2026-07-13-02";
+const CACHE_NAME = "rml-pliage-v69-2026-08-20";
 
 const FILES_TO_CACHE = [
   "./",
@@ -10,47 +10,29 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-        )
-      )
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
-  if(event.request.mode === "navigate"){
+  if (event.request.mode === "navigate") {
     event.respondWith(
       fetch(event.request)
         .then(response => {
           const copy = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(cache => cache.put("./index.html", copy));
-
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
           return response;
         })
         .catch(() => caches.match("./index.html"))
     );
-
     return;
   }
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
